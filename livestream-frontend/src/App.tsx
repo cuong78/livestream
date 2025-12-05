0a4ee823fb57c782da8fd0086fd592898f04b711
anhCuong <cuongcaoleanh@gmail.com>
2025-12-05 18:16:19 +0700

    fix: remove unused admin routes from the application

--- livestream-frontend/src/App.tsx
+++ livestream-frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ViewerPage from "./pages/ViewerPage";
-import AdminLoginPage from "./pages/AdminLoginPage";
-import AdminDashboardPage from "./pages/AdminDashboardPage";
 import StreamSettingsPage from "./pages/StreamSettingsPage";
 
 function App() {
@@ -9,9 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<ViewerPage />} />
-        <Route path="/admin/login" element={<AdminLoginPage />} />
-        <Route path="/admin" element={<AdminDashboardPage />} />
-        <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
         <Route path="/admin/stream-settings" element={<StreamSettingsPage />} />
       </Routes>
     </Router>
